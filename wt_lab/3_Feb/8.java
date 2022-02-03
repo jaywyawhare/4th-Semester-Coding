a86316a2fb0e29c26027bea9ae2805f274799eb7
Arinjay <2005441@kiit.ac.in>
2022-02-03 17:29:49 +0530

    Q 8

--- wt_lab/3_Feb/8.java
+++ wt_lab/3_Feb/8.java
@@ -1,24 +1,22 @@
-// Write a program in Java to initialize a number and count the no. of dissimilar digits from that number. (Ex- 4548, no. of digits=3)
-
 import java.util.Scanner;
-public static 8 {
+
+public class 8 {
     public static void main(String[] args) {
-        int digits[] = new int[10];
+        int digs[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        System.out.println("Enter a number: ");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        int temp = num;
-        int count = 0;
+        int number = sc.nextInt();
+        int temp = number;
         while (temp > 0) {
-            int a = temp % 10;
-            digits[a]++;
-            temp = temp / 10;
+            digs[temp % 10] = 1;
+            temp /= 10;
         }
-        for (int i = 0; i < 10; i++) {
-            if (digits[i] > 1) {
-                count++;
-            }
+        int digits = 0;
+        for (int i = 0; i < digs.length; i++) {
+
+            digits += digs[i];
         }
-        System.out.println("The number of dissimilar digits is: " + count);
+        System.out.println("no of distinct digits are: " + digits);
+        sc.close();
     }
 }
